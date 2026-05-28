febee852ea937077445a020a3f27f72d44b579ba
pratyush-who <pratyushmehra2005@gmail.com>
2026-05-28 18:39:45 +0530

    init

---
+++ src/main/java/com/example/renma/RenmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.renma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/renma/RenmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
